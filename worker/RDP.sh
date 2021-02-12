b02df7d0c54a28c0d79d43670625bdb23662b3ad
Juan <72490350+jusidama18@users.noreply.github.com>
2021-02-12 16:43:08 +0700

    Update RDP.sh

--- worker/RDP.sh
+++ worker/RDP.sh
@@ -33,6 +33,8 @@ wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.t
 tar -xf xdm-setup-7.2.11.tar.xz
 sudo ./install.sh
 cd /content/
+sudo dpkg --add-architecture i386
+sudo apt-get install libc-ares2
 sudo add-apt-repository -y ppa:atareao/telegram
 sudo apt update && sudo apt install telegram
 sudo add-apt-repository -y ppa:gnome3-team/gnome3
@@ -41,6 +43,7 @@ sudo apt-get install gnome-session-flashback
 sudo apt install ffmpeg
 sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo apt install obs-studio
+sudo apt-get update
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
